29a0f1cc42c647a2d1f4d2843b009d6c77756c25
unknown <rajlodhgi@gmail.com>
2022-05-27 08:04:22 +0530

    Added @app

--- templates/main.py
+++ templates/main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+   
+@app.route('/predict')
+def home():
+    return render_template('index.html') 
+
 if __name__ == ('__main__'):
     app.run(debug=True)
 
